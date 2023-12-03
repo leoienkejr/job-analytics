60365fe66c17d794efa2c140a930e9c8fdc8fcce
leoienkejr <leonardoienkejr@gmail.com>
2023-12-03 00:22:47 -0300

    Change CodeBuild service allow, adding permission to list and delete S3 objects

--- terraform/cicd_pipeline/resources.tf
+++ terraform/cicd_pipeline/resources.tf
@@ -45,6 +45,10 @@ data "aws_iam_policy_document" "deployment_pipeline_role_policy" {
       "s3:GetBucketVersioning",
       "s3:PutObjectAcl",
       "s3:PutObject",
+      "s3:ListBucket",
+      "s3:ListObjectsV2",
+      "s3:DeleteObject",
+      "s3:DeleteObjects"
     ]
 
     resources = [
