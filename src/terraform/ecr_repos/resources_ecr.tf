f846229dad8eb97fd1bb86ef2fe748c1cd7ffb72
leoienkejr <leonardoienkejr@gmail.com>
2024-03-30 20:10:47 -0300

    Fix name repository name for LinkedinExtractor_ECRRepository

--- src/terraform/ecr_repos/resources_ecr.tf
+++ src/terraform/ecr_repos/resources_ecr.tf
@@ -1,5 +1,5 @@
-resource "aws_ecr_repository" "LinkedinExtractorECRRepository" {
-  name                 = "LinkedinExtractor"
+resource "aws_ecr_repository" "LinkedinExtractor_ECRRepository" {
+  name                 = "linkedin-extractor"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
