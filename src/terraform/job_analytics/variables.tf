a984b068dac7b42cad0c87d53c230ad7ab90d23a
leoienkejr <leonardoienkejr@gmail.com>
2024-03-30 20:14:38 -0300

    Remove variable 'CodeBuildServiceRole' from job_analytics stack

--- src/terraform/job_analytics/variables.tf
+++ src/terraform/job_analytics/variables.tf
@@ -2,8 +2,3 @@ variable "aws_region" {
   type    = string
   default = "us-east-1"
 }
-
-variable "CodeBuildServiceRole" {
-  type = string
-  description = "ARN of the role used by the CodeBuild project. Necessary in order to give the role permissions to interact with the ECR repositories."
-}
