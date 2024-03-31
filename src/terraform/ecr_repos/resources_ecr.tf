802ceac78f9fe94d2b37b2d9278ed65ad57a8038
leoienkejr <leonardoienkejr@gmail.com>
2024-03-31 17:42:31 -0300

    Add lifecycle policy to the 'linkedin-extractor' ECR repository

--- src/terraform/ecr_repos/resources_ecr.tf
+++ src/terraform/ecr_repos/resources_ecr.tf
@@ -6,3 +6,28 @@ resource "aws_ecr_repository" "LinkedinExtractor_ECRRepository" {
     scan_on_push = true
   }
 }
+
+
+resource "aws_ecr_lifecycle_policy" "LinkedinExtractor_ECRRepositoryPolicy" {
+  repository = aws_ecr_repository.LinkedinExtractor_ECRRepository.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 14 days",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 14
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
